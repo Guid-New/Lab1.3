3c0b7c6c1dac483a4d2a66646bc3905373c6ea0f
Guid-New <nauserko@gmail.com>
2020-05-01 23:28:26 +0300

    Added Builder

---
+++ Laba1_2/HardwareClasses/HardwareSpecsBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_2.Logic
+{
+    public class HardwareSpecsBuilder
+    {
+        private HardwareSpecs _hardwareSpecs = new HardwareSpecs();
+
+        public HardwareSpecsBuilder SetClockSpeed(double Ghz)
+        {
+            _hardwareSpecs.ClockSpeed = Ghz;
+            return this;
+        }
+
+        public HardwareSpecsBuilder SetInternetConnection(bool option)
+        {
+            _hardwareSpecs.InternetConnection = option;
+            return this;
+        }
+
+        public HardwareSpecsBuilder SetHardwareType(HardwareType type)
+        {
+            _hardwareSpecs.Type = type;
+            return this;
+        }
+
+        public HardwareSpecsBuilder SetCoresCount(int count)
+        {
+            _hardwareSpecs.Cores = count;
+            return this;
+        }
+
+        public HardwareSpecsBuilder SetFreeSpace(int count)
+        {
+            _hardwareSpecs.FreeSpace = count;
+            return this;
+        }
+
+        public HardwareSpecsBuilder SetRam(int count)
+        {
+            _hardwareSpecs.Ram = count;
+            return this;
+        }
+
+        public void Reset()
+        {
+            _hardwareSpecs = new HardwareSpecs();
+        }
+
+        public HardwareSpecs GetHardware()
+        {
+            return _hardwareSpecs;
+        }
+    }
+}
---
+++ Laba1_2/HardwareClasses/HardwareSpecsDirector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_2.Logic.HardwareClasses
+{
+    class HardwareSpecsDirector
+    {
+        private readonly HardwareSpecsBuilder _builder = new HardwareSpecsBuilder();
+
+        public HardwareSpecs CreateUltimateGamingRig()
+        {
+            return _builder.SetHardwareType(HardwareType.PC)
+                .SetCoresCount(12)
+                .SetClockSpeed(5)
+                .SetFreeSpace(10000000)
+                .SetInternetConnection(true)
+                .SetRam(64)
+                .GetHardware();
+        }
+
+        public HardwareSpecs CreateConsole()
+        {
+            return _builder.SetHardwareType(HardwareType.PC)
+                .SetCoresCount(8)
+                .SetClockSpeed(2)
+                .SetFreeSpace(1000)
+                .SetInternetConnection(true)
+                .SetRam(8)
+                .GetHardware();
+        }
+
+        public HardwareSpecs CreateMobile()
+        {
+            return _builder.SetHardwareType(HardwareType.PC)
+                .SetCoresCount(8)
+                .SetClockSpeed(2.3)
+                .SetFreeSpace(64)
+                .SetInternetConnection(true)
+                .SetRam(8)
+                .GetHardware();
+        }
+    }
+}
